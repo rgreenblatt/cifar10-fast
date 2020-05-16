8c8b06f059b29383234cc1310ef3e319ab0998ed
rgreenblatt <greenblattryan@gmail.com>
2020-05-16 10:22:56 -0400

    Format

--- dawn_utils.py
+++ dawn_utils.py
@@ -1,14 +1,19 @@
 from torch_backend import *
 
+
 #Network definition
 def conv_bn_default(c_in, c_out, pool=None):
     block = {
-        'conv': nn.Conv2d(c_in, c_out, kernel_size=3, stride=1, padding=1, bias=False), 
-        'bn': BatchNorm(c_out), 
-        'relu': nn.ReLU(True)
+        'conv':
+        nn.Conv2d(c_in, c_out, kernel_size=3, stride=1, padding=1, bias=False),
+        'bn':
+        BatchNorm(c_out),
+        'relu':
+        nn.ReLU(True)
     }
     if pool: block['pool'] = pool
     return block
+
 
 def residual(c, conv_bn, **kw):
     return {
@@ -18,8 +23,20 @@ def residual(c, conv_bn, **kw):
         'add': (Add(), ['in', 'res2/relu']),
     }
 
-def net(channels=None, weight=0.125, pool=nn.MaxPool2d(2), extra_layers=(), res_layers=('layer1', 'layer3'), conv_bn=conv_bn_default, prep=conv_bn_default):
-    channels = channels or {'prep': 64, 'layer1': 128, 'layer2': 256, 'layer3': 512}
+
+def net(channels=None,
+        weight=0.125,
+        pool=nn.MaxPool2d(2),
+        extra_layers=(),
+        res_layers=('layer1', 'layer3'),
+        conv_bn=conv_bn_default,
+        prep=conv_bn_default):
+    channels = channels or {
+        'prep': 64,
+        'layer1': 128,
+        'layer2': 256,
+        'layer3': 512
+    }
     n = {
         'input': (None, []),
         'prep': prep(3, channels['prep']),
@@ -34,9 +51,13 @@ def net(channels=None, weight=0.125, pool=nn.MaxPool2d(2), extra_layers=(), res_
     for layer in res_layers:
         n[layer]['residual'] = residual(channels[layer], conv_bn)
     for layer in extra_layers:
-        n[layer]['extra'] = conv_bn(channels[layer], channels[layer])       
+        n[layer]['extra'] = conv_bn(channels[layer], channels[layer])
     return n
 
+
 def tsv(logs):
-    data = [(output['epoch'], output['total time']/3600, output['valid']['acc']*100) for output in logs]
-    return '\n'.join(['epoch\thours\ttop1Accuracy']+[f'{epoch}\t{hours:.8f}\t{acc:.2f}' for (epoch, hours, acc) in data])
+    data = [(output['epoch'], output['total time'] / 3600,
+             output['valid']['acc'] * 100) for output in logs]
+    return '\n'.join(
+        ['epoch\thours\ttop1Accuracy'] +
+        [f'{epoch}\t{hours:.8f}\t{acc:.2f}' for (epoch, hours, acc) in data])
